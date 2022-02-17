92871cdce3b63e20086fdb656f576b4c9d1b2648
Macyl23 <ahmedzaidmacyl99@gmail.com>
2022-02-17 15:13:26 +0100

    Fonction signIN qui permet de se connecter
    - Fonction signIN qui contrôle si l'adresse mail existe dans la BD ensuite qui décrypte le mot de passe et compare avec le mdp correspondant a l'email
    - Affiche dans la console "Client trouvé" et qui renvoire l'id du user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const UserModel= require("../models/userModel");
+const bcrypt= require('bcrypt');
+
 
 
 // fonction qui permet de créer son compte
@@ -11,5 +13,21 @@ module.exports.signUP= async(req,res) => {
     }
     catch(err){
         res.status(200).send({err})
+    }
+}
+
+module.exports.signIN = async(req , res ) => {
+    const {email , password} = req.body
+    try{
+        const user= await UserModel.findOne({email : email});
+        if(user) {
+            const auth= await bcrypt.compare(password , user.password);
+            if(auth){
+                console.log("Client trouvé");
+                res.status(200).json({user: user._id})
+            }
+        }
+    }catch(err){
+        res.status(200).json(err);
     }
 }
