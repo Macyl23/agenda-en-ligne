80738e23521477b285b436c67a1581ca0e75149e
Macyl23 <ahmedzaidmacyl99@gmail.com>
2022-02-17 16:58:34 +0100

    Ajout de la route vers la fonction checkUser
    - Une fois l'utlisateur connecté ça  permet le contrôle que le user est conencté sur toutes les pages du site

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
 const bodyParser=require('body-parser')
+const cookieParser= require('cookie-parser');
 require('dotenv').config({path: './config/.env'})
 require('./db');
 
 const indexRouter = require("./routes/index")
 const userRouter= require("./routes/user.routes");
+const { checkUser } = require('./middleware/authmiddleware');
 
 // Instructions qui permet de parser les informations du user
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser());
+
+//jwt
+app.use('*', checkUser);
 
 
 //Accés aux pages
