ef7655e961b75bda28e0065c4978e946f23f62d1
Macyl23 <ahmedzaidmacyl99@gmail.com>
2022-02-17 14:53:20 +0100

    Cryptage du mot de passe
    - En utilisant le package bcrypt on crypte le mdp du client à la création du compte en utilisant userSchema.pre

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
 require('mongoose-type-email')
+const bcrypt = require('bcrypt');
 
 
 // Schéma d'un client
@@ -49,5 +50,14 @@ const userSchema= new mongoose.Schema(
     },
     {typeKey: '$type'}
 )
+
+//crypter le mot de passe
+userSchema.pre("save", async function(next){
+    const salt= await bcrypt.genSalt();
+    this.password= await bcrypt.hash(this.password , salt);
+    next();
+})
+
+
 const UserModel= mongoose.model('user', userSchema)
 module.exports= UserModel
